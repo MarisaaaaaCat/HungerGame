688c32e7b2ee5b868d38b1f106e69d7b8a6b3a0f
JimmyLIn <linyuan5856139@163.com>
2018-05-09 01:24:07 +0800

    demo creat player and scene

---
+++ Client/Assets/Script/CreateScene.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using Script;
+using UnityEngine;
+
+public class CreateScene : MonoBehaviour
+{
+	private List<Vector2> positions;
+	private GameObject root;
+	private const int scale = 100;
+	
+	void Start () {
+		this.InitScene();
+		this.CreatRoad(300);
+		this.CreatPlayer();
+		this.CreatGiant();
+	}
+
+
+	void InitScene()
+	{
+		if (root==null)
+		{
+			root=new GameObject("Root");
+			root.transform.position=new Vector3(0f,0f,100f);
+		}
+	}
+	
+	void CreatRoad(int nums)
+	{
+		for (int i = 0; i < nums; i++)
+		{
+			CreatCube();
+		}
+	}
+	
+	void CreatCube()
+	{	
+		positions=new List<Vector2>();
+		var pos = Random.insideUnitCircle;
+		if (!positions.Contains(pos))
+		{
+			var go=GameObject.CreatePrimitive(PrimitiveType.Cube);
+			var targetPos = pos * scale;
+			go.transform.position = new Vector3(targetPos.x,0f,targetPos.y);	
+			this.positions.Add(pos);
+			go.transform.SetParent(root.transform,false);
+		}	
+	}
+
+	void CreatPlayer()
+	{
+		Vector2 playerPos = this.positions[Random.Range(0, this.positions.Count)];
+		var go=GameObject.CreatePrimitive(PrimitiveType.Cube);
+		go.name = "Player";
+		var targetPos = playerPos * scale;
+		go.transform.position = new  Vector3(targetPos.x,1f,targetPos.y);
+		go.transform.localScale=new Vector3(.5f,.5f,.5f);
+		go.transform.SetParent(root.transform,false);
+		go.AddComponent<Rigidbody>();
+		go.GetComponent<MeshRenderer>().material.color=Color.red;
+		go.AddComponent<Player>();
+	}
+
+	void CreatGiant()
+	{
+		var go=GameObject.CreatePrimitive(PrimitiveType.Cube);
+		go.name = "Giant";
+		go.transform.localScale=new Vector3(3,10,3);
+		go.transform.position = Vector3.zero;
+		go.transform.SetParent(root.transform,false);
+		go.GetComponent<MeshRenderer>().material.color=Color.black;
+	}
+
+	private const string GUIContent = "Press Space To Jump \n W front \n S back \n A left  \n D right";
+	void OnGUI()
+	{
+		GUI.color=Color.black;
+		GUI.TextArea(new Rect(10f,0f,150f,150f),GUIContent);
+	}
+	
+}
---
+++ Client/Assets/Script/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Script
+{
+    public class Player : MonoBehaviour
+    {
+        private Camera mainCamera;
+
+        private Rigidbody _rigidbody;
+        private float x;
+        private float y;
+        [SerializeField] private float speed=0.1f;
+        [SerializeField] private float jumpSpeed = .1f;
+        [SerializeField] private bool ground;
+        void Start()
+        {
+            if (mainCamera == null)
+            {
+                mainCamera = Camera.main;
+                mainCamera.transform.position = this.transform.position + Vector3.back * 10 + Vector3.up * 5f;
+            }
+
+            this._rigidbody = this.GetComponent<Rigidbody>();
+        }
+
+
+        void Update()
+        {
+            if (ground)
+            {
+                x = Input.GetAxis("Horizontal");
+                y = Input.GetAxis("Vertical");
+                Vector3 offset = new Vector3(x, 0f, y)*speed;
+                this.transform.position += offset;  
+            }
+           
+          
+            mainCamera.transform.LookAt(this.transform);
+        }
+
+        void FixedUpdate()
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+        if (Input.GetKey(KeyCode.Space))
+        {
+                if (ground)
+                {   
+                    transform.Translate(new Vector3(x, 0f,y)*speed);
+                    this._rigidbody.velocity += new Vector3(0, 5, 0);
+                    this._rigidbody.AddForce(Vector3.up * jumpSpeed);
+                    ground = false;
+                   
+                }
+            }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            this.ground = true;
+            other.gameObject.GetComponent<MeshRenderer>().material.color=Color.yellow;
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            other.gameObject.GetComponent<MeshRenderer>().material.color=Color.white;
+        }
+    }
+}
